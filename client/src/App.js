2c6036472a99fac446789d467f619d2420681a18
WadeeKT <thomassanna1e@gmail.com>
2023-06-05 10:41:06 +0200

    Login - Register (coding)

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,15 @@
+const Register = () => {
+    return(
+        <div>
+            <h1>Register</h1>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <input type="password" placeholder="Confirm Password" />
+                <button>Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register;
